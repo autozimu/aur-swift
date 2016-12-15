0ec6735dca26e6064db4bfbeaa6b0b5e3a3618bd
Junfeng Li <autozimu@gmail.com>
2016-12-14 22:22:03 -0500

    Turn on build of swift.

--- build.sh
+++ build.sh
@@ -9,9 +9,10 @@ chown nobody .
 sudo -u nobody makepkg
 pacman --noconfirm -U libkqueue-2.1.0-1-x86_64.pkg.tar.xz
 
-# cd /tmp
-# curl https://aur.archlinux.org/cgit/aur.git/snapshot/swift-language.tar.gz > swift-language.tar.gz
-# tar -xvzf swift-language.tar.gz
-# cd swift-language
-# chown nobody .
-# sudo -u nobody makepkg
+cd /tmp
+curl https://aur.archlinux.org/cgit/aur.git/snapshot/swift-language.tar.gz > swift-language.tar.gz
+tar -xvzf swift-language.tar.gz
+cd swift-language
+chown nobody .
+sudo -u nobody makepkg
+ls -l
